006612eece39c0e58952bc0789340f5bebd1a96b
mahbubur39 <mahbubur39@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-09-19 21:33:07 +0000

    git-svn-id: https://plugins.svn.wordpress.org/post-meta/tags/1.2@601294 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- lib/views/mamageTaxonomies.php
+++ lib/views/mamageTaxonomies.php
@@ -5,14 +5,14 @@ global $pluginCore;
 
 <div class="wrap">
     <div id="pm-icon-edit-pages" class="icon32 icon32-posts-page"><br /></div>  
-    <h2>Custopm Taxonomy Management Panel</h2>   
+    <h2>Custom Taxonomy Management Panel</h2>   
     <?php do_action( 'mo_notice' ); ?>
     <div id="dashboard-widgets-wrap">
         <div class="metabox-holder">
                 <div id="mo_post_taxonomy_list">
                         <?php  
                         echo $pluginCore->render( 'metaBox', array( 
-                                        'title'     => "Manage Texonomies", 
+                                        'title'     => "Manage Taxonomies", 
                                         'content'   => $pluginCore->pmTaxonomyList(), 
                                         'deleteIcon'=> false,
                                         'isOpen'    => true,
