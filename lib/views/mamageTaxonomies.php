5eff1f3d41bf85578701c98d2158bf3d1739e291
mahbubur39 <mahbubur39@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-09-19 21:05:36 +0000

    git-svn-id: https://plugins.svn.wordpress.org/post-meta/tags/1.2@601267 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ lib/views/mamageTaxonomies.php
@@ -0,0 +1,32 @@
+<?php
+global $pluginCore;
+
+?>
+
+<div class="wrap">
+    <div id="pm-icon-edit-pages" class="icon32 icon32-posts-page"><br /></div>  
+    <h2>Custopm Texonomy Management Panel</h2>   
+    <?php do_action( 'mo_notice' ); ?>
+    <div id="dashboard-widgets-wrap">
+        <div class="metabox-holder">
+                <div id="mo_post_taxonomy_list">
+                        <?php  
+                        echo $pluginCore->render( 'metaBox', array( 
+                                        'title'     => "Manage Texonomies", 
+                                        'content'   => $pluginCore->pmTaxonomyList(), 
+                                        'deleteIcon'=> false,
+                                        'isOpen'    => true,
+                                        'toggle'     =>  true
+                                    ) );
+                        ?>
+                </div>
+                <div id="mo_loading_area">
+                        <?php  
+                         echo $pluginCore->render('taxonomy');
+                        ?>
+                </div>
+        </div>
+    </div>     
+</div>
+
+                    
