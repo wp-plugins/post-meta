5e675042b766da8081e7b4ba19ac44a1099de214
mahbubur39 <mahbubur39@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-09-20 21:50:18 +0000

    git-svn-id: https://plugins.svn.wordpress.org/post-meta/trunk@601777 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ lib/controllers/pmSettingsController.php
@@ -0,0 +1,34 @@
+<?php
+
+if( !class_exists( 'pmSettingsController' ) ) :
+    class pmSettingsController {
+        
+        function __construct() {
+           // add_action('admin_menu', array( $this, 'admin_menu' )); 
+           
+            
+        }
+        function admin_menu(){
+             //$page =add_submenu_page( 'meta_option', 'Manage Meta Settings', 'Settings', 'administrator', 'meta_option_settings', array( $this, 'meta_option_settings' ));
+             //add_action('admin_print_styles-' . $page,array( $this,'meta_option_sc'));
+        }
+        function meta_option_settings(){
+            global $pluginCore;
+            
+             $pluginCore->render("settings", $settings); 
+        }
+        function meta_option_sc(){            
+            wp_register_style( 'meta-option-admin-style', PM_ASSECTS_URL.'/css/meta_option_admin.css' );
+            wp_enqueue_style('meta-option-admin-style' );
+             
+            wp_register_script( 'meta-option-admin-script', PM_ASSECTS_URL.'/js/meta_option_admin.js'); 
+            wp_enqueue_script('meta-option-admin-script' ); 
+        
+                 
+        }
+
+}
+    
+endif;
+
+?>
