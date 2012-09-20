05b8583788cbf59caa0bd734c6c9ee1774895640
mahbubur39 <mahbubur39@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-09-20 21:50:18 +0000

    git-svn-id: https://plugins.svn.wordpress.org/post-meta/tags/1.3@601777 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ assets/js/meta_option_admin.js
@@ -0,0 +1,452 @@
+/* Post type Manage Page  */
+
+function pmUpdatePostType(element){
+    jQuery(element).validationEngine();
+    if( !jQuery(element).validationEngine("validate") ) {
+                    
+                    jQuery('.pc_error').remove(); 
+                    jQuery('.pc_success').remove(); 
+                    }else{
+                        arg=jQuery( element ).serialize();
+                        bindElement =jQuery('#submit');
+                        jQuery('.pc_error').remove(); 
+                        jQuery('.pc_success').remove();
+                        
+                        AjaxCall( bindElement, 'pm_post_type_update', arg, function(data){
+                            jQuery('.msg').children(".pc_error").remove();
+                            jQuery('.msg').html(data);
+                            //jQuery('#pm_posttype_menu').append("<div class='mo_type_manage_option'><div class='mo_type_option'><b>{$post_type['type']}</b><span class='mo_edit_link'> <a href='#Edit' rel='$key' onclick='editPostType(this); return false;' >Edit</a> | | <a href='#Delete' rel='$key' onclick='deletePostType(this); return false;' >Delete</a></span></div></div>");
+                             window.location.reload(true);
+                        });
+                        
+                        /*JsonAjaxCall( jQuery(element), "pm_post_type_update", '', function(data){
+                                    jQuery('.msg').children(".pc_error").remove();
+                                    jQuery('.msg').html(data.msg);
+                                    jQuery('#pm_posttype_menu').append(data.menu);
+                        });*/
+                        
+                        
+                    }
+    
+}
+
+
+function editPostType(element){
+    
+    arg = "post_type_key="+jQuery(element).attr('rel');
+    AjaxCall( jQuery(element), 'pm_post_type_edit', arg, function(data){
+        jQuery("#mo_loading_area").html( data ); 
+    });
+    
+}
+
+function deletePostType(element){
+    if( confirm('Confirm to remove?') ){
+        arg = "post_type_key="+jQuery(element).attr('rel');
+        AjaxCall( jQuery(element), 'pm_post_type_delete', arg, function(data){
+            jQuery(element).parents('.mo_edit_link').parents('.mo_type_option').remove(); 
+            window.location.reload(true);
+        });
+    }
+}
+
+function addPostType(element){
+    AjaxCall( jQuery(element), 'pm_post_type_add','', function(data){
+        jQuery("#mo_loading_area").html( data ); 
+    });
+}
+
+
+function pmPostTypeSuggetion(element){
+    
+        jQuery('#advanced-label input[name*=pm_posttype]:text').each(function(index,value) {
+              rel = jQuery(this).attr('rel');
+              label = jQuery('#pm_posttype_name').val();
+              if(!rel){
+                rel =label;
+              }else{
+                
+                if(rel=='All'){
+                    rel='All '+label;
+                }else if(rel=='Add'){
+                    rel='Add '+label;
+                }else if(rel=='All New'){
+                    rel='All New '+label;
+                }else if(rel=='Add'){
+                    rel='Edit '+label;
+                }else if(rel=='Edit'){
+                    rel='Edit '+label;
+                }else if(rel=='New'){
+                    rel='New '+label;
+                }else if(rel=='View'){
+                    rel='View '+label;
+                }else if(rel=='No found'){
+                    rel='No '+label+' found';
+                }else if(rel=='No found in Trash'){
+                    rel='No '+label+' found in Trash';
+                }else if (rel =='Parent:'){
+                    rel='Parent '+label+':';
+                }else{
+                    rel=jQuery(this).attr('rel')+label;
+                }
+                
+              }
+              jQuery(this).val(rel);
+            });
+            jQuery('#pm_posttype_label').val(jQuery(element).val()+'s');
+    
+}
+    
+
+
+/*  End */
+
+/* Taxonomy manage page */
+function pmUpdateTaxonomy(element){
+    jQuery(element).validationEngine();
+                    if( !jQuery(element).validationEngine("validate") ) {
+                    
+                        jQuery('.pc_error').remove(); 
+                        jQuery('.pc_success').remove(); 
+                    }else{
+                        arg=jQuery( element ).serialize();
+                        bindElement = jQuery('#submit');
+                        jQuery('.pc_error').remove(); 
+                        jQuery('.pc_success').remove();
+                        
+                        AjaxCall( bindElement, 'pm_taxonomy_update', arg, function(data){
+                            jQuery('.msg').children(".pc_error").remove();
+                            jQuery('.msg').html(data);
+                            //jQuery('#pm_posttype_menu').append("<div class='mo_type_manage_option'><div class='mo_type_option'><b>{$post_type['type']}</b><span class='mo_edit_link'> <a href='#Edit' rel='$key' onclick='editPostType(this); return false;' >Edit</a> | | <a href='#Delete' rel='$key' onclick='deletePostType(this); return false;' >Delete</a></span></div></div>");
+                             //window.location.reload(true);
+                        });
+                        
+                        /*JsonAjaxCall( jQuery(element), "pm_post_type_update", '', function(data){
+                                    jQuery('.msg').children(".pc_error").remove();
+                                    jQuery('.msg').html(data.msg);
+                                    jQuery('#pm_posttype_menu').append(data.menu);
+                        });*/
+                        
+                        
+                    }
+    
+}
+
+function editTaxonomy(element){
+    
+    arg = "taxonomy_key="+jQuery(element).attr('rel');
+    AjaxCall( jQuery(element), 'pm_taxonomy_edit', arg, function(data){
+        jQuery("#mo_loading_area").html( data ); 
+    });
+    
+}
+
+function deleteTaxonomy(element){
+    if( confirm('Confirm to remove?') ){
+        arg = "taxonomy_key="+jQuery(element).attr('rel');
+        AjaxCall( jQuery(element), 'pm_taxonomy_delete', arg, function(data){
+            jQuery(element).parents('.mo_edit_link').parents('.mo_type_option').remove(); 
+        });
+    }
+}
+
+function addTaxonomy(element){
+    AjaxCall( jQuery(element), 'pm_taxonomy_add', '', function(data){
+        jQuery("#mo_loading_area").html( data ); 
+    });
+}
+
+
+function pmTaxonomySuggetion(element){
+    jQuery('#advanced-label input[name*=pm_taxonomy]:text').each(function(index,value) {
+              rel = jQuery(this).attr('rel');
+              label = jQuery('#pm_taxonomy_name').val();
+              if(!rel){
+                rel =label;
+              }else{
+                if(rel=='Search'){
+                    rel='Search '+label;
+                }else if(rel=='Add New'){
+                    rel='Add New '+label;
+                }else if(rel=='Popular'){
+                    rel='Popular '+label;
+                }else if(rel=='All'){
+                    rel='All '+label;
+                }else if(rel=='Parent'){
+                    rel='Parent '+label;
+                }else if(rel=='Parent:'){
+                    rel='Parent '+label+':';
+                }else if(rel=='Edit'){
+                    rel='Edit '+label;
+                }else if(rel=='Update'){
+                    rel='Update '+label;
+                }else if(rel=='New Name'){
+                    rel='New '+label+' Name';
+                }else if(rel=='Separate  with commas'){
+                    rel='Separate '+label+' with commas';
+                }else if(rel=='Choose from the most used'){
+                    rel='Choose from the most used '+label;
+                }else{
+                    rel=jQuery(this).attr('rel')+label;
+                }
+                
+              }
+              jQuery(this).val(rel);
+            });
+            jQuery('#pm_taxonomy_label').val(jQuery(element).val()+'s');
+    
+}
+
+/* End Taxonomy */
+jQuery('.mo_field_meta_key').click(function(){
+        
+        label=jQuery(this).parents('.inside').children('.mo_field_segment').children('.mo_field_label');
+        group_meta_key=jQuery(this).parents('.inside').parents('.inside').children('.meta_box_info').children('.mo_segment').children('.mo_group_meta_key').val();
+        //alert(group_meta_key);
+          if (label.val().length > 0 && jQuery(this).val() == '') {
+            label.stringToSlug({
+              space:'_',
+              getPut:jQuery(this), 
+              prefix:group_meta_key + " ",
+              replace:/\s?\([^\)]*\)/gi
+            });
+          }
+        
+    });
+    jQuery('.mo_field_label').click(function(){
+        
+        meta_key=jQuery(this).parents('.inside').children('.mo_field_segment').children('.mo_field_meta_key');
+        group_meta_key=jQuery(this).parents('.inside').parents('.inside').children('.meta_box_info').children('.mo_segment').children('.mo_group_meta_key').val();
+        alert(jQuery(this).val());
+          if (jQuery(this).val().length > 0) {
+            jQuery(this).stringToSlug({
+              space:'_',
+              getPut:meta_key, 
+              prefix:group_meta_key + " ",
+              replace:/\s?\([^\)]*\)/gi
+            });
+          }
+        
+    });
+
+ function meta_option_toggole_field(toggleIcon){
+    jQuery(toggleIcon).parents('.box').children('.inside').toggle();
+ }
+ function meta_option_toggole(toggleIcon){
+    jQuery(toggleIcon).parents('.postbox').children('.inside').toggle();
+ }
+ 
+function meta_option_remove_field( removeIcon ){
+    if( confirm('Confirm to remove?') ){
+        jQuery(removeIcon).parents('.postbox').parents('.meta-field').remove();
+    }    
+}
+function meta_option_remove_meta_box( removeIcon ){
+    if( confirm('Confirm to remove?') ){
+        jQuery(removeIcon).parents('.postbox').parents('.meta-box-sortables').remove();
+    }    
+}
+
+function moNewGroup( element ){
+    
+        jQuery('#not-found').remove();
+        newGroupID = parseInt( jQuery('#last_group_id').val() ) + 1;
+        arg = 'id=' + newGroupID + '&toggle='+ true + '&post_type=' + jQuery(element).attr('post_type');
+        AjaxCall( element, 'mo_add_group', arg, function(data){
+        jQuery("#mo_group_container").append( data );
+        jQuery('#last_group_id').val( newGroupID );
+        });
+    //newID = parseInt( jQuery("#last_id").val() ) + 1;
+    //arg = 'field_type=' + jQuery(element).attr('field_type');
+    //alert(arg);
+    /*
+    pfAjaxCall( element, 'um_add_field', arg, function(data){
+        jQuery("#um_fields_container").append( data );        
+        jQuery("#last_id").val( newID );
+    });*/
+}
+
+
+function manageGroup( element ){
+        arg = "post_type="+jQuery(element).attr('rel');
+        
+    AjaxCall( element, 'mo_manage_group', arg, function(data){
+        jQuery("#mo_loading_area").html( data ); 
+    });
+}
+
+function moNewField( element ){ 
+    group_id = jQuery(element).attr('group_id');
+    post_type= jQuery(element).attr('post_type');
+    field_type= jQuery(element).attr('field_type');
+    last_field_id =jQuery("#last_"+group_id+"_field_id").val();//jQuery(".field_count_"+group_id+":last").val();
+    newFieldID = parseInt( last_field_id ) + 1;
+    arg = "group_id="+group_id+"&post_type="+post_type+"&field_type="+field_type+"&field_id="+newFieldID;
+    //alert(field_type);
+    AjaxCall( element, 'mo_add_field', arg, function(data){
+        jQuery('#not-found').remove();
+        jQuery(element).parents('.metabox-option-list').children(".metabox-fields-holder").append( data );
+        jQuery("#last_"+group_id+"_field_id").val(newFieldID);   
+    });
+}
+
+function moChangeGroupTitle( element ){
+    title = jQuery(element).val();
+    if( !title ){ title = 'Untitled Group'; }
+    jQuery(element).parents(".postbox").children("h3").text(title);
+}
+function moChangeFieldTitle( element ){
+  
+    title = jQuery(element).val();
+    if( !title ){ title = 'Untitled Field'; }
+    jQuery(element).parents(".fieldbox").children("h3").children(".mo_admin_field_title").text(title);
+            meta_key=jQuery(element).parents('.field-holder').children('.mo_field_segment').children('.mo_field_meta_key');
+            jQuery(element).parents(".fieldbox").children("h3").children(".mo_metakey_handl").children('.mo_meta_key').text(meta_key.val());
+        group_meta_key=jQuery(element).parents('.inside').parents('.inside').children('.meta_box_info').children('.mo_segment').children('.mo_group_meta_key').val();
+            jQuery(element).stringToSlug({
+              space:'_',
+              getPut: meta_key, 
+              prefix: group_meta_key + " ",
+              replace:/\s?\([^\)]*\)/gi
+            });
+}
+
+function moChangeFieldMetaKey( element ){
+    metakey = jQuery(element).val();
+    jQuery(element).parents(".fieldbox").children("h3").children('.mo_metakey_handl').children(".mo_meta_key").text(metakey);
+}
+
+function meta_option_group_save( element ){
+    
+    jQuery(element).validationEngine();
+    if( jQuery(element).validationEngine("validate") ){
+                    //jQuery(".inside").css('display','none');
+                    jQuery('.pc_error').remove(); 
+                    jQuery('.pc_success').remove();               
+                    bindElement = jQuery("#submit");
+                    mo_post_type = jQuery("#mo_post_type").val();
+                    arg = jQuery( element ).serialize()+"&post_type="+mo_post_type;
+                    //alert(arg);
+                    AjaxCall( bindElement, 'meta_option_group_save', arg, function(data){
+                        jQuery('#msg').children(".pc_error").remove();
+                        jQuery('#msg').html(data);
+                    });
+    }else{
+        jQuery(".inside").css('display','block');
+        jQuery('.pc_success').remove();
+        jQuery('#msg').html('<div class="pc_error"> Some Required field need to fill </div>');
+                    return false; 
+    }
+    /*if (!jQuery(element).validate({
+                ignore:'',
+                errorClass: "pc_error",
+                errorElement:"div"}).form())
+                {
+                    jQuery('.pc_success').remove();
+                    return false; //doesn't validate
+                }else
+                {
+                    jQuery('.pc_error').remove(); 
+                    jQuery('.pc_success').remove();               
+                    bindElement = jQuery("#submit");
+                    mo_post_type = jQuery("#mo_post_type").val();
+                    arg = jQuery( element ).serialize()+"&post_type="+mo_post_type;
+                    //alert(arg);
+                    AjaxCall( bindElement, 'meta_option_group_save', arg, function(data){
+                        bindElement.parent().children(".mo_ajax").remove();
+                        bindElement.after("<div class='mo_ajax'>"+data+"</div>");
+                    });
+                }
+
+       /* if(!jQuery(element).validate({
+                ignore:'',
+                errorClass: "pc_error",
+                errorElement:"div",
+            invalidHandler: function(form, validator) { 
+              var errors = validator.numberOfInvalids();
+              if (errors) {
+                jQuery('.pc_success').remove();
+                return false;
+                    
+              }
+            }
+      }))return;
+      alert('hi');
+      /*
+      jQuery('.pc_error').remove(); 
+                 jQuery('.pc_success').remove();               
+                bindElement = jQuery("#submit");
+                mo_post_type = jQuery("#mo_post_type").val();
+                arg = jQuery( element ).serialize()+"&post_type="+mo_post_type;
+                //alert(arg);
+               AjaxCall( bindElement, 'meta_option_group_save', arg, function(data){
+                    bindElement.parent().children(".mo_ajax").remove();
+                    bindElement.after("<div class='mo_ajax'>"+data+"</div>");
+                });*/
+}
+function moChangeField(element ,fieldID, groupID){
+    arg = jQuery('#field' + groupID + fieldID + ' * ').serialize();
+    AjaxCall( element, "mo_change_field", arg, function(data){
+        jQuery(element).parents(".meta-field").replaceWith(data);
+    });
+}
+
+function pmSuggetion(element){
+    jQuery('#advanced-label input[name*=pm_posttype]:text').each(function(index,value) {
+              rel = jQuery(this).attr('rel');
+              label = jQuery('#pm_posttype_labal').val();
+              //rel = str_replace('%s',label,rel);//
+              rel=rel.replace('%s',label);
+             // rel=jQuery(this).attr('rel')+label;
+              jQuery(this).val(rel);
+            });
+}
+
+
+
+
+
+
+
+function AjaxCall( element, action, arg, handle){
+    if(action) data = "action=" + action;
+    if(arg)    data = arg + "&action=" + action;
+    if(arg && !action) data = arg;
+    data = data ;
+    //data = data + "&pf_nonce=" + pf_nonce + "&is_ajax=true";
+    //alert(data);
+    //if( typeof(ajaxurl) == 'undefined' ) ajaxurl = front.ajaxurl;
+
+	jQuery.ajax({
+		type: "post",
+        url: ajaxurl,
+        data: data,
+		beforeSend: function() { jQuery("<span class='mo_loading'></span>").insertAfter(element); },
+		success: function( data ){
+            jQuery(".mo_loading").remove();
+            handle(data);
+		}
+	});    
+}
+
+
+function JsonAjaxCall( element, action, arg, handle){
+	jQuery.ajax({
+		type: "post",
+        url: ajaxurl,
+        data: {
+            'action':action
+            },
+        dataType:'JSON',            
+		beforeSend: function() { jQuery("<span class='mo_loading'></span>").insertAfter(element); },
+        success:function(data){
+                    jQuery(".mo_loading").remove();
+                        handle(data);
+                             },
+          error: function(errorThrown){
+               alert('error');
+               console.log(errorThrown);
+          }
+	});    
+}
+
---
+++ lib/models/pmHelpModel.php
@@ -0,0 +1,195 @@
+<?php
+if( !class_exists( 'pmHelpModel' ) ) :
+
+class pmHelpModel {
+    
+    
+    /**
+     * @ This function will retuen All Custom meta key which is created ny Meta Optopn plugin 
+     */
+    
+     function getMetaKeyList(){
+        global $postMeta,$pluginCore;
+        
+            $posttypes=$pluginCore->pm_get_post_types();
+            $html = '';
+            
+            $html .= "<div class='mo_meta_key_list'>";
+                  foreach($posttypes as  $pt):
+            
+                        $data = get_option($postMeta->options[$pt->name]);
+                       $html .="<h4>Meta Key For $pt->name </h4>";
+                       
+                        if($data['group']){
+                           foreach($data['group'] as $group){
+                                if($group['field']){
+                                        foreach($group['field'] as $field){
+                                            $html .="<ul>"; 
+                                                $html .="<li>{$field['meta_key']}</li>";
+                                            $html .="</ul>"; 
+                                        }
+                                    }else{$html .= "No Custom Field Found";}
+                            } 
+                        }else{ $html .= "No Custom Group Found";}
+                          
+                    endforeach;
+                    
+            $html .="</div>";
+            return $html;
+    }
+    
+    function getMetaKeyUse(){
+        
+        $html ="";
+        
+        $html .= '< ?php $meta_values = get _post_meta($post_id, <b>$meta_key</b>, $single); ?>  ';
+        $html .= '<br /> <br />OR <br /> <br />< ?php $meta_values = get _post_meta($post->ID, <b>$meta_key</b>, $single); ?>  ';
+        
+        $html .='
+                <p>
+                <h4>$post_id</h4>
+                    (integer) (required) The ID of the post from which you want the data. Use $post->ID to get a post\'s ID.
+                
+                        Default: None
+                </p>
+                <p>
+                <h4>$key</h4>
+                    (string) (optional) A string containing the name of the meta value you want.
+                
+                        Default: empty string
+                <p>
+                </p>
+                <h4>$singlem</h4>
+                    (boolean) (optional) If set to true then the function will return a single result, as a string. If false, or not set, then the function returns an array of the custom fields. This may not be intuitive in the context of serialized arrays. If you fetch a serialized array with this method you want $single to be true to actually get an unserialized array back. If you pass in false, or leave it out, you will have an array of one, and the value at index 0 will be the serialized string.
+                
+                        Default: false
+                </p>';
+                
+        return $html;
+        
+    }
+    
+    function manageType($type=null){
+        global $pluginCore;
+        $html ="";
+        if($type=="post_type"):
+        /*
+        <div class='mo_type_manage'>
+                                <div class='mo_edit'></div>
+                                <div class='mo_delete'></div>
+                            </div>
+        */
+             //$html .= "<span post_type='$pt->label' class='button add-new-h2 mo_add_button' onclick='moNewGroup(this);' >Add New</span>";
+              $posttypes=$pluginCore->pm_get_post_types();
+              foreach($posttypes as  $pt):
+              $html .="<div class='mo_type_manage_option'>
+                            
+                            <div class='mo_type_option'>
+                                <b>$pt->label</b><small>({$pt->labels->menu_name})</small>
+                                <span class='mo_edit_link'>
+                                    <a href='#ManaagePostMeta' rel='$pt->name' onclick='manageGroup(this); return false;' >Manage Group/Fields</a>
+                                </span>
+                            </div>
+                        </div>";
+             endforeach;
+            
+            
+       elseif($type=="taxonomy"):
+              $posttypes=$pluginCore->pm_get_post_types();
+              foreach($posttypes as  $pt):
+              $html .="<div class='mo_type_manage_option'>
+                            <div class='mo_type_option'>
+                                <b>$pt->label</b><small>({$pt->labels->menu_name})</small>
+                                <span class='mo_edit_link'>
+                                    <a href='admin.php?page=meta_option&section=form_custom_group&type=$pt->name' rel='$pt->name' onclick='manageGroup(this); return false;' >Manage</a>
+                                </span>
+                            </div>
+                        </div>";
+             endforeach;
+      endif; 
+              
+         return $html;
+    }
+    
+    function pmPostTypeList(){
+        global $postMeta;
+        $pm_post_types = get_option( $postMeta->options['post_types'] );
+        $html ='';
+        $html .='<div id="pm_posttype_menu">';
+        if($pm_post_types){
+            foreach($pm_post_types as $key => $post_type){
+                $html .="<div class='mo_type_manage_option'>
+                            
+                            <div class='mo_type_option'>
+                                <b>{$post_type['name']}</b>
+                                <span class='mo_edit_link'>
+                                    <a href='#Edit' rel='$key' onclick='editPostType(this); return false;' >Edit</a> | | <a href='#Delete' rel='$key' onclick='deletePostType(this); return false;' >Delete</a>
+                                </span>
+                            </div>
+                        </div>";
+            }
+        }else{
+            $html .="<div class='pm_nodata'> No Custom Post Type found</div>";
+        }
+        $html .="</div>";
+        $html .="<input type='button' class='button button-primary pm_menu_button pm_menu_button' onclick='addPostType(this); return false;' value='Add New Post Type' />";
+        
+        return $html;
+    }
+    
+    function pmTaxonomyList(){
+        global $postMeta;
+        $pm_taxonomies = get_option( $postMeta->options['taxonomies'] );
+        $html ='';
+        $html .='<div id="pm_posttype_menu">';
+        if($pm_taxonomies){
+            foreach($pm_taxonomies as $key => $pm_taxonomy){
+                $html .="<div class='mo_type_manage_option'>
+                            
+                            <div class='mo_type_option'>
+                                <b>{$pm_taxonomy[name]}</b>
+                                <span class='mo_edit_link'>
+                                    <a href='#Edit' rel='$key' onclick='editTaxonomy(this); return false;' >Edit</a> | | <a href='#Delete' rel='$key' onclick='deleteTaxonomy(this); return false;' >Delete</a>
+                                </span>
+                            </div>
+                        </div>";
+            }
+        }else{
+            $html .="<div class='pm_nodata'> No Custom Taxonomy found</div>";
+        }
+        $html .="</div>";
+        $html .="<input type='button' class='button button-primary pm_menu_button' onclick='addTaxonomy(this); return false;' value='Add New Taxonomy' />";
+        
+        return $html;
+    }
+    
+    
+    function featuredData(){
+        $html ='';
+        $html .='<h4>Current Feature:</h4>
+                    <ul>
+                        <li>1. Fully jQuery and ajax based Management</li>
+                        <li>2. Group and Field</li>
+                        <li>3. Sorting group and field by dragging</li>
+                        <li>4. Managing Custom Post type</li>
+                        <li>5. Managing Custom Taxonomy</li>
+                        <li>6. It build for All user(Developer and user)</li>
+                    </ul>
+                ';
+        $html .='<h4>Coming Feature:</h4>
+                    <ul>
+                        <li>1. Will add more custom field</li>
+                        <li>2. Will add more custom feature for both(Developer and user)user</li>
+                        <li>3. Will add short code </li>
+                    </ul>
+                ';
+                
+                
+        $html .='<div style="padding:10px"><i><b>Please help me to develop a useful plugin by giving me advice and guideline</b></i></div>';
+         return $html;
+        
+    }
+}
+
+endif;
+?>
